90bd3313f16a38d456e14d42bb0ff0c812ec2ad5
Joseph Stanton <stantoje@sunyit.edu>
2015-10-18 11:52:48 -0400

    initial code checkin

---
+++ src/CheckingAccount.java
@@ -0,0 +1,41 @@
+
+public class CheckingAccount {
+    /**
+     * 1. Store the amount money in the account
+     */
+
+    public CheckingAccount(){
+        this(0.0);
+    }
+
+    public CheckingAccount(Double amount){
+        /**
+         * 2. Set the amount of money in the account to amount
+         */
+    }
+
+    public void deposit(Double amount){
+        /**
+         * 3. Deposits amount into the account
+         */
+    }
+
+    public double withdraw(Double amount){
+        /**
+         * 4. Checks that the user has enough money in their account
+         * if they have enough, deduct it from their account
+         * if they don't, execute the following code
+         throw new RuntimeException("Sorry, you don't have enough money in your account");
+         */
+
+
+        return amount;
+    }
+
+    public double getBalance(){
+        /**
+         * 5. Return the current balance of the account
+         */
+        return 0.0;
+    }
+}
---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+    public static void main(String[] args){
+        /**
+         * 6. Test out the checking account
+         */
+        CheckingAccount alice = new CheckingAccount(500.0);
+
+        CheckingAccount bob = new CheckingAccount();
+
+
+
+    }
+}
