024fb7d5c2d2adeed6fe16ce59761068e3717ae7
Joseph Stanton <stantoje@sunyit.edu>
2015-10-18 20:50:03 -0400

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Assignment-3
+# Assignment 3
+### Description
+This assignment focuses on reinforcing the following topics:
+* Basic Variables and Arithmetic
+* If Statements
+* Basic Method Calls
+
+### Instructions
+CheckingAccount.java:
+1. Store the amount money in the account
+2. Set the amount of money in the account to amount
+3. Deposits amount into the account
+4. Checks that the user has enough money in their account
+5. Return the current balance of the account
+
+Driver.java:
+6. Withdraw $120 from alice's account
+7. Deposit $20 into bob's account
+8. check that alice has more funds in her account than bob
+
+### Grading
+This assignment will be graded based on completion of the following points:
+
+* No compilation errors - (4)
+* 1 - (2)
+* 2 - (2)
+* 3 - (2)
+* 4 - (2)
+* 5 - (2)
+* 6 - (2)
+* 7 - (2)
+* 8 - (2)
+
+Total Points: 20
--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,31 @@
 public class Driver {
+    /**
+     * Remember to call methods on an object, use <ObjectName>.<MethodName>(parameters list)
+     * For example:
+     *  Math.sin(3.141598)
+     *  alice.getBalance()
+     */
     public static void main(String[] args){
         /**
-         * 6. Test out the checking account
+         * Test out the checking account
          */
         CheckingAccount alice = new CheckingAccount(500.0);
 
+        //6. Withdraw $120 from alice's account
+        //Insert code here
+
+        assert(alice.getBalance() == 380.0);
+
+        //Note: bob's account will start out empty
         CheckingAccount bob = new CheckingAccount();
 
+        //7. Deposit $20 into bob's account
+        //Insert code here
 
+        assert(bob.getBalance() == 20.0);
 
+        //8. check that alice has more funds in her account than bob
+
+        //assert(/* Uncomment and insert code here */);
     }
 }
